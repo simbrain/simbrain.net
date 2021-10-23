9d2434c4e88b976bd536fd755e97144e9a18b186
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2021-10-23 12:02:25 -0700

    Removing .sh files from repo

--- put_v3_docs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DOCDIR="/Users/jyoshimi/gitstuff/simbrain3/docs/"
-rsync $DOCDIR -avz --delete --exclude=".*" -e ssh jyoshimi@jeffyoshimi.net:public_html/simbrain/Documentation/v3/
--- put_v4_docs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DOCDIR="/Users/jyoshimi/gitstuff/simbrain/docs/"
-rsync $DOCDIR -avz --delete --exclude=".*" -e ssh jyoshimi@jeffyoshimi.net:public_html/simbrain/Documentation/v4/
